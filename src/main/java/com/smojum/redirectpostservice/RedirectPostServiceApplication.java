26f5cd6eb7c2a24d9263bf778b88987f03775eee
smojum <santanu.mojumder@gmail.com>
2019-04-02 09:49:41 -0500

    First commit

---
+++ src/main/java/com/smojum/redirectpostservice/RedirectPostServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smojum.redirectpostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedirectPostServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedirectPostServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smojum/redirectpostservice/RedirectPostServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smojum.redirectpostservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedirectPostServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
